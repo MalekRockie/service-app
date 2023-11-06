8b40bda36c21eab8e437e7d492c7eb0cea335466
kdudley9 <98002234+kdudley9@users.noreply.github.com>
2023-11-06 16:26:08 -0500

    Add routes to App.js, update background color to black and text color to white

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,17 @@
 import './App.css';
-import Navbar from "./components/Navbar";
 import ServiceProviderProfile from "./components/ServiceProviderProfile";
-import { BrowserRouter as Router,Routes, Route} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 import Chat from "./components/Chat";
 import Rating from "./components/Rating";
 
 function App() {
   return (
     <div className="App">
-      <Router>
         <Routes>
           <Route path="/service-provider-profile" element={<ServiceProviderProfile />} />
           <Route path="/chat" element={<Chat />} />
           <Route path="/rating" element={<Rating />} />
         </Routes>
-      </Router>
     </div>
   );
 }
