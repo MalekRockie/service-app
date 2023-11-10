8b87f808e713b1e09e8369d3dc540dcd98bdfed8
kdudley9 <98002234+kdudley9@users.noreply.github.com>
2023-11-09 19:14:57 -0500

    Update routes, add blank review and chat components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,17 @@ import './App.css';
 import ServiceProviderProfile from "./components/ServiceProviderProfile";
 import {Routes, Route} from 'react-router-dom';
 import ServiceBrowser from "./components/ServiceBrowser";
+import Reviews from "./components/Reviews";
+import Chat from "./components/Chat";
 
 function App() {
   return (
     <div className="App">
         <Routes>
           <Route path="/service-provider-profile/:id" element={<ServiceProviderProfile />} />
+          <Route path="/service-provider/:id/reviews" element={<Reviews />} />
           <Route path="/service-providers" element={<ServiceBrowser />} />
+          <Route path="/chat" element={<Chat />} />
         </Routes>
     </div>
   );
---
+++ frontend/src/components/Reviews.js
@@ -0,0 +1,12 @@
+import Navbar from "./Navbar";
+
+const Reviews = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1> Review page </h1>
+    </div>
+  );
+}
+
+export default Reviews;
