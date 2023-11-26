7bdec36bfded77ca0a4c5efd99d49cfae9bc9333
kdudley9 <98002234+kdudley9@users.noreply.github.com>
2023-11-26 18:00:04 -0500

    Update navbar icon functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import {Routes, Route} from 'react-router-dom';
 import ServiceBrowser from "./components/ServiceBrowser";
 import Reviews from "./components/Reviews";
 import Chat from "./components/Chat";
+import Orders from "./components/Orders";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/service-provider-profile/:id" element={<ServiceProviderProfile />} />
           <Route path="/service-provider/:id/reviews" element={<Reviews />} />
+          <Route path="/service-provider/:id/orders" element={<Orders />} />
           <Route path="/service-providers" element={<ServiceBrowser />} />
           <Route path="/chat" element={<Chat />} />
         </Routes>
